e5f1008e1a2d7898f8082252a40abc7fee9d33b3
Imigrance <mathias.nyrup@live.dk>
2024-09-18 13:33:58 +0200

    tick and delta time implemented

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -15,6 +18,9 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
+        
+        tick = clock.tick(60)
+        dt = tick/1000
 
 if __name__ == "__main__":
     main()
